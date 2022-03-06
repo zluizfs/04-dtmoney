55c38c543b1707f15abbbd763e595dcd63112b7a
Luiz Fernando de Souza <zluizfs@outlook.com>
2022-03-06 19:14:18 -0300

    Criação de interface e fake api

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => console.log(response.data))
+  }, [])
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>06/03/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.000</td>
+            <td>Casa</td>
+            <td>02/03/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
