2d8a168701df93122e2875c1f999b578c898ade3
Luiz Fernando de Souza <zluizfs@outlook.com>
2022-04-30 17:56:58 -0300

    Adicionado transaction model

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      
       <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
