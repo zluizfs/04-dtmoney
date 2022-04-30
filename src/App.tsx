87a30a9d26827e7b48fa032b44aa051ea16c6b73
Luiz Fernando de Souza <luizfs@unipam.edu.br>
2022-04-30 19:25:44 -0300

    Contextos

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from "react-modal";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -21,17 +22,17 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
-      
+
       <Dashboard />
 
-      <NewTransactionModal 
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
